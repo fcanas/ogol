a8987f504ad4d9deabfd469a0b58c007b07dc959
Fabián Cañas <fcanas@gmail.com>
2020-04-14 17:46:41 -0400

    Convert segments into multisegments

--- Sources/LogoLang/TurtleGraphics.swift
+++ Sources/LogoLang/TurtleGraphics.swift
@@ -48,90 +48,111 @@ public struct Turtle {
         public let end: Point
     }
 
+    public typealias MultiLine = Array<Turtle.Segment>
+
     public static let defaultAngle: Double = -90
 
-    public var position: Point = .zero
-    public var angle: Double = Turtle.defaultAngle
-    public var pen: Pen = .down
-    public var visible: Bool = true
+    public var position: Point
+    public var angle: Double
+    public var pen: Pen
+    public var visible: Bool
+    public var multiline: MultiLine
 
     public init(position: Point = .zero,
                 angle: Double = Turtle.defaultAngle,
                 pen: Pen = .down,
-                visible: Bool = true
+                visible: Bool = true,
+                multiline: MultiLine = []
         ) {
         self.position = position
         self.angle = angle
         self.pen = pen
         self.visible = visible
+        self.multiline = multiline
     }
 
     func with(position: Point? = nil,
               angle: Double? = nil,
               pen: Pen? = nil,
-              visible: Bool? = nil
+              visible: Bool? = nil,
+              multiline: MultiLine? = nil
         ) -> Turtle {
         return Turtle(position: position ?? self.position,
                       angle: angle ?? self.angle,
                       pen: pen ?? self.pen,
-                      visible: visible ?? self.visible
+                      visible: visible ?? self.visible,
+                      multiline: multiline ?? self.multiline
         )
     }
 
-    func performing(_ command: Turtle.Command) -> (Turtle, Segment?) {
+    func performing(_ command: Turtle.Command) -> (Turtle, MultiLine?) {
         let out = with(command)
-        let segment: Segment?
+        let lineOut: MultiLine?
 
         switch command {
 
-        case .fd(_) where self.pen == .down, .bk(_)  where self.pen == .down:
-            segment = Segment(start: position, end: out.position)
+        case .pu, .setxy(_), .home:
+            lineOut = multiline
         default:
-            segment = nil
+            lineOut = nil
         }
 
-        return (out, segment)
+        return (out, lineOut)
     }
 
     func with(_ command: Turtle.Command) -> Turtle {
         switch command {
         case let .fd(dist):
             let a = angle * .pi / 180
-            return with(position: position + Point(x: cos(a) * dist, y: sin(a) * dist))
+            let multiLine: MultiLine
+            let newPosition = position + Point(x: cos(a) * dist, y: sin(a) * dist)
+            if pen == .down {
+                multiLine = self.multiline + [Segment(start: position, end: newPosition)]
+            } else {
+                multiLine = self.multiline
+            }
+            return with(position: newPosition, multiline: multiLine)
         case let .bk(dist):
             let a = angle * .pi / 180
-            return with(position: position - Point(x: cos(a) * dist, y: sin(a) * dist))
+            let multiLine: MultiLine
+            let newPosition = position - Point(x: cos(a) * dist, y: sin(a) * dist)
+            if pen == .down {
+                multiLine = self.multiline + [Segment(start: position, end: newPosition)]
+            } else {
+                multiLine = self.multiline
+            }
+            return with(position: newPosition, multiline: multiLine)
         case let .lt(a):
             return with(angle: angle + a)
         case let .rt(a):
             return with(angle: angle - a)
         case .pu:
-            return with(pen: .up)
+            return with(pen: .up, multiline: [])
         case .pd:
             return with(pen: .down)
         case .home:
-            return with(position:.zero, angle: Turtle.defaultAngle)
+            return with(position:.zero, angle: Turtle.defaultAngle, multiline: [])
         case .st:
             return with(visible: true)
         case .ht:
             return with(visible: false)
         case let .setxy(position):
-            return with(position: position)
+            return with(position: position, multiline: [])
         }
     }
 }
 
 public struct Canvas {
     public let turtle: Turtle
-    public let segments: [Turtle.Segment]
+    public let multiLines: [Turtle.MultiLine]
 
-    public init(turtle: Turtle, segments: [Turtle.Segment] = []) {
+    public init(turtle: Turtle, multiLines: [Turtle.MultiLine] = []) {
         self.turtle = turtle
-        self.segments = segments
+        self.multiLines = multiLines
     }
 
     public func performing(_ command: Turtle.Command) -> Canvas {
-        let (t, s) = turtle.performing(command)
-        return Canvas(turtle: t, segments: s.map { self.segments + [$0] } ?? segments )
+        let (t, m) = turtle.performing(command)
+        return Canvas(turtle: t, multiLines: m.map { self.multiLines + [$0] } ?? multiLines )
     }
 }
