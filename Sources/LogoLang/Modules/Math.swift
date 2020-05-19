8b91d3ec44219912e1d55278221744c79a102240
Fabián Cañas <fcanas@gmail.com>
2020-05-18 20:59:59 -0400

    Add random and abs

--- Sources/LogoLang/Modules/Math.swift
+++ Sources/LogoLang/Modules/Math.swift
@@ -1,6 +1,6 @@
 //
 //  Math.swift
-//  FFCParserCombinator
+//  LogoLang
 //
 //  Created by Fabián Cañas on 5/17/20.
 //
@@ -13,44 +13,64 @@ public protocol Module {
 
 public struct LogoMath: Module {
 
-    private static let nativeFunctions: [String:(Double)->Double] = [
-        "cos" : Darwin.cos,
-        "sin" : Darwin.sin,
-        "tan" : Darwin.tan,
-        "acos" : Darwin.acos,
-        "asin" : Darwin.asin,
-        "atan" : Darwin.atan,
+    private enum SingleParameter: Module {
+        private static let nativeFunctions: [String:(Double)->Double] = [
+            "cos" : Darwin.cos,
+            "sin" : Darwin.sin,
+            "tan" : Darwin.tan,
+            "acos" : Darwin.acos,
+            "asin" : Darwin.asin,
+            "atan" : Darwin.atan,
 
-        "cosh" : Darwin.cosh,
-        "sinh" : Darwin.sinh,
-        "tanh" : Darwin.tanh,
-        "acosh" : Darwin.acosh,
-        "asinh" : Darwin.asinh,
-        "atanh" : Darwin.atanh,
+            "cosh" : Darwin.cosh,
+            "sinh" : Darwin.sinh,
+            "tanh" : Darwin.tanh,
+            "acosh" : Darwin.acosh,
+            "asinh" : Darwin.asinh,
+            "atanh" : Darwin.atanh,
 
-        "exp" : Darwin.exp,
-        "exp2" : Darwin.exp2,
+            "exp" : Darwin.exp,
+            "exp2" : Darwin.exp2,
 
-        "lgamma" : Darwin.lgamma,
-        "tgamma" : Darwin.tgamma,
+            "lgamma" : Darwin.lgamma,
+            "tgamma" : Darwin.tgamma,
 
-        "log" : Darwin.log,
-        "log10" : Darwin.log10,
-        "log2" : Darwin.log2,
-    ]
+            "log" : Darwin.log,
+            "log10" : Darwin.log10,
+            "log2" : Darwin.log2,
+            "abs" : Darwin.fabs,
+            "sqrt" : Darwin.sqrt,
+        ]
+
+        public static let procedures: [String : NativeProcedure] = {
+            var out: [String:NativeProcedure] = [:]
+            nativeFunctions.forEach { (key: String, function: @escaping (Double) -> Double) in
+                out[key] = NativeProcedure(name: key, parameters: [Value.deref("LogoMathParam")]) { (params, context) throws -> Bottom? in
+                    guard case let .double(param) = params.first else {
+                        throw ExecutionHandoff.error(.parameter, "\(key) needs a numeric parameter")
+                    }
+                    return Bottom.double(function(param))
+                }
+            }
+            return out
+        }()
+    }
+
+    private enum Random: Module {
+        public static let procedures: [String : NativeProcedure] = {
+            return ["random":NativeProcedure(name: "random", parameters: [Value.deref("top")], action: { (params, _) -> Bottom? in
+                guard case let .double(param) = params.first else {
+                    throw ExecutionHandoff.error(.parameter, "random needs a numeric parameter")
+                }
+                return .double(Double(arc4random() % UInt32(param)))
+            })]
+        }()
+    }
 
     public static let procedures: [String : NativeProcedure] = {
-        var out: [String:NativeProcedure] = [:]
-        nativeFunctions.forEach { (key: String, function: @escaping (Double) -> Double) in
-            out[key] = NativeProcedure(name: key, parameters: [Value.deref("LogoMathParam")]) { (params, context) throws -> Bottom? in
-                guard case let .double(param) = params.first else {
-                    throw ExecutionHandoff.error(.parameter, "\(key) needs a numeric parameter")
-                }
-                return Bottom.double(function(param))
-            }
-        }
-        return out
+        return SingleParameter.procedures.merging(Random.procedures, uniquingKeysWith: { (a,b) in a })
     }()
+
 }
 
 
