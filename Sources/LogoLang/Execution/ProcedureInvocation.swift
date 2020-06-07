c729f70cd950cdd557155f97e43cea947bdc6b52
Fabián Cañas <fcanas@gmail.com>
2020-06-06 22:04:34 -0400

    Move ProcedureInvocation to its own file

---
+++ Sources/LogoLang/Execution/ProcedureInvocation.swift
@@ -0,0 +1,71 @@
+//
+//  ProcedureInvocation.swift
+//  LogoLang
+//
+//  Created by Fabian Canas on 6/6/20.
+//
+
+import Foundation
+
+public struct ProcedureInvocation: ExecutionNode, Equatable {
+
+    let name: String
+    let parameters: [Value]
+    
+    public func evaluateParameters(in context: ExecutionContext) throws -> (Procedure, Dictionary<String,Bottom>) {
+        guard let procedure = context.procedures[name] else {
+            throw ExecutionHandoff.error(.missingSymbol, "I don't know how to \(name)")
+        }
+        
+        guard procedure.parameters.count == parameters.count else {
+            throw ExecutionHandoff.error(.parameter, "\(name) needs \(procedure.parameters.count) parameters. I found \(parameters.count).")
+        }
+        
+        var parameterMap: Dictionary<String,Bottom> = Dictionary(minimumCapacity: parameters.count)
+        for (index, parameter) in parameters.enumerated() {
+            parameterMap[procedure.parameters[index]] = try parameter.evaluate(context: context)
+        }
+        
+        return (procedure, parameterMap)
+    }
+    
+    public func execute(context: ExecutionContext, reuseScope: Bool) throws {
+        
+        let (procedure, parameterMap) = try evaluateParameters(in: context)
+        
+        let newScope: ExecutionContext
+        if reuseScope {
+            context.inject(procedures: procedure.procedures)
+            parameterMap.forEach { (key: String, value: Bottom) in
+                context.variables[key] = value
+            }
+            newScope = context
+        } else {
+            newScope = try ExecutionContext(parent: context, procedures: procedure.procedures, variables: parameterMap)
+        }
+        
+        try procedure.execute(context: newScope, reuseScope: reuseScope)
+        
+    }
+}
+
+extension ProcedureInvocation: Evaluatable {
+    public var description: String {
+        return "p:->" + name
+    }
+
+    public func evaluate(context: ExecutionContext) throws -> Bottom {
+        do {
+            try execute(context: context, reuseScope: false)
+        } catch let ExecutionHandoff.output(bottom) {
+            return bottom
+        }
+        throw ExecutionHandoff.error(.noOutput, "No value returned from \(self).")
+    }
+}
+
+extension ProcedureInvocation: SyntaxColorable {
+    public func syntaxCategory() -> SyntaxCategory? {
+        return .procedureInvocation
+    }
+}
